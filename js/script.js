199b26d4182e737589781638242f1e957c867bdd
zhLisha <lisazhou1709@gmail.com>
2022-06-15 16:07:21 +0200

    created grid for each levels

--- js/script.js
+++ js/script.js
@@ -16,6 +16,12 @@ const bntStartGame = document.querySelector('#start-game');
 bntStartGame.addEventListener('click', startGame)
 
 function startGame() {
+    // Elementi HTML 
+    const mainGrid = document.querySelector('#grid');
+
+    // Svuotare la griglia ad nuova selezione del livello 
+    mainGrid.innerHTML = '';
+
     // Variabile numero massimo bombe
     const maxBombs = 16;
 
@@ -29,15 +35,21 @@ function startGame() {
     // Se sceglie lv.Medium i numeri sono compresi tra 1-81
     // Se sceglie lv.Hard i numeri sono compresi tra 1-49
 
+
+    // Variabile per l'aggiunta della classe 
+    let classSquare;
     // Variabile numero massimo per ogni livello
     let maxNumber;
 
     if(userLv === 'easy') {
         maxNumber = 100;
+        classSquare = 'easy';
     } else if(userLv === 'medium') {
         maxNumber = 81;
+        classSquare = 'medium';
     } else if(userLv === 'hard') {
         maxNumber = 49;
+        classSquare = 'hard';
     }
 
     console.log(maxNumber);
@@ -54,9 +66,23 @@ function startGame() {
     console.log('Tentativi massimi:', maxAttempts);
 
     // Generatore della griglia in base al maxNumber 
-    const maxSquare = generateSquare();
+    generateSquare();
+    function generateSquare() {
+        // Ciclo per produrre i numeri in base a maxNumber 
+        for(let i = 1; i <= maxNumber; i++){
+            // Popolare la mainGrid con i contenuti del maxNumber
+            const newDiv = document.createElement('div');
+            newDiv.innerHTML = `<span>${i}</span>`;
+            newDiv.classList.add('squares');
 
+             // // Aggiungere una classe per creare le dimensioni dei quadrati 
+            newDiv.classList.add(classSquare);
+            console.log(mainGrid);
 
+            // Appendere alla nostra mainGrid
+            mainGrid.append(newDiv);
+        }
+    }
 }
 
 
@@ -95,9 +121,6 @@ function bombsNumber(minNumberLv, maxNumberLv, totalNumberElement) {
 }
 
 
-function generateSquare() {
-
-}
 
 
 
